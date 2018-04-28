e33c3436eeb558485db3a2964980813519bf53b7
cryptostack <stacyalme@me.com>
2018-04-28 03:09:34 -0500

    practice

---
+++ deck.py
@@ -0,0 +1,41 @@
+52 cards
+    s1     d1     c1    h1
+    s2     d2     c2    h2
+    s3     d3     c3    h3
+    s4     d4     c4    h4
+    s5     d5     c5    h5
+    s6     d6     c6    h6
+    s7     d7     c7    h7
+    s8     d8     c8    h8
+    s9     d9     c9    h9
+    s10    d10    c10   h10 
+    s11    d11    c11   h11
+    s12    d12    c12   h12   
+    s13    d13    c13   h13              
+
+72
+48
+=
+120
+
+128/52 = 2.461
+
+def deck_to_number(deck):
+
+    for 0 to end of deck
+
+
+deck_to_number(deck) = 
+for i = 0 to 127 {
+shuffle deck
+pull a card 
+128bit_number(i) = (card value) mod 2 
+put card back into deck
+} 
+return 128bit_number 
+
+
+deck_to_number(deck): 
+
+
+
